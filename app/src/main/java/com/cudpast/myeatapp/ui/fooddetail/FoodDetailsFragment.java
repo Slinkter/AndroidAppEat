ef150747c221155a2aa5d1c2fde653355b9cbc40
Slinkter <luis.j.cueva@gmail.com>
2020-04-04 11:10:00 -0500

    v08:fin

--- app/src/main/java/com/cudpast/myeatapp/ui/fooddetail/FoodDetailsFragment.java
+++ app/src/main/java/com/cudpast/myeatapp/ui/fooddetail/FoodDetailsFragment.java
@@ -1,5 +1,7 @@
 package com.cudpast.myeatapp.ui.fooddetail;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -11,21 +13,79 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
+import com.andremion.counterfab.CounterFab;
+import com.bumptech.glide.Glide;
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.cudpast.myeatapp.Commom.Common;
+import com.cudpast.myeatapp.Model.FoodModel;
 import com.cudpast.myeatapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class FoodDetailsFragment extends Fragment {
 
     private FoodDetailsViewModel mViewModel;
+
+    public Unbinder unbinder;
+
+    @BindView(R.id.btnCart)
+    CounterFab btnCart;
+    @BindView(R.id.btn_rating)
+    FloatingActionButton btn_rating;
+    @BindView(R.id.food_name)
+    TextView food_name;
+    @BindView(R.id.food_description)
+    TextView food_description;
+    @BindView(R.id.food_price)
+    TextView food_price;
+    @BindView(R.id.number_button)
+    ElegantNumberButton numberButton;
+    @BindView(R.id.ratingBar)
+    RatingBar ratingBar;
+    @BindView(R.id.btnShowComment)
+    Button btnShowCommnet;
+    @BindView(R.id.img_food)
+    ImageView img_food;
+
 
     public static FoodDetailsFragment newInstance() {
         return new FoodDetailsFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.food_details_fragment, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_food_detail, container, false);
+        unbinder = ButterKnife.bind(this, root);
+        mViewModel = ViewModelProviders.of(this).get(FoodDetailsViewModel.class);
+        mViewModel.getMutableLiveDataFood().observe(this, new Observer<FoodModel>() {
+            @Override
+            public void onChanged(FoodModel foodModel) {
+                displayInfo(foodModel);
+            }
+        });
+
+        return root;
     }
+
+    private void displayInfo(FoodModel foodModel) {
+        Glide.with(getContext()).load(foodModel.getImage()).into(img_food);
+        food_name.setText(new StringBuffer(foodModel.getName()));
+        food_description.setText(new StringBuilder(foodModel.getDescription()));
+        food_price.setText(new StringBuilder(foodModel.getPrice().toString()));
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(Common.selectedFood.getName());
+
+    }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
--- app/src/main/java/com/cudpast/myeatapp/ui/fooddetail/FoodDetailsViewModel.java
+++ app/src/main/java/com/cudpast/myeatapp/ui/fooddetail/FoodDetailsViewModel.java
@@ -1,7 +1,28 @@
 package com.cudpast.myeatapp.ui.fooddetail;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.cudpast.myeatapp.Commom.Common;
+import com.cudpast.myeatapp.Model.FoodModel;
+
 public class FoodDetailsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+
+    private MutableLiveData<FoodModel> mutableLiveDataFood;
+
+    public FoodDetailsViewModel() {
+    }
+
+    public MutableLiveData<FoodModel> getMutableLiveDataFood() {
+
+        if (mutableLiveDataFood == null){
+            mutableLiveDataFood = new MutableLiveData<>();
+        }
+        mutableLiveDataFood.setValue(Common.selectedFood);
+
+
+
+        return mutableLiveDataFood;
+    }
 }
