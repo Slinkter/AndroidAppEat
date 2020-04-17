99b9861495e7c8425cb6d8b6abcb8887097eef43
Slinkter <luis.j.cueva@gmail.com>
2020-04-17 15:38:59 -0500

    v14:fin

--- app/src/main/java/com/cudpast/myeatapp/Adapter/MyFoodListAdapter.java
+++ app/src/main/java/com/cudpast/myeatapp/Adapter/MyFoodListAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,7 +14,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.cudpast.myeatapp.Callback.IRecyclerClickListener;
 import com.cudpast.myeatapp.Commom.Common;
+import com.cudpast.myeatapp.Database.CartDataSource;
+import com.cudpast.myeatapp.Database.CartDatabase;
 import com.cudpast.myeatapp.Database.CartItem;
+import com.cudpast.myeatapp.Database.LocalCartDataSource;
+import com.cudpast.myeatapp.EventBus.CounterCartEvent;
 import com.cudpast.myeatapp.EventBus.FoodItemClick;
 import com.cudpast.myeatapp.Model.FoodModel;
 import com.cudpast.myeatapp.R;
@@ -26,15 +31,24 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MyFoodListAdapter extends RecyclerView.Adapter<MyFoodListAdapter.MyViewHolder> {
 
     private Context context;
     private List<FoodModel> foodModelList;
+    private CompositeDisposable compositeDisposable;
+    private CartDataSource cartDataSource;
+
 
     public MyFoodListAdapter(Context context, List<FoodModel> foodModelList) {
         this.context = context;
         this.foodModelList = foodModelList;
+        this.compositeDisposable = new CompositeDisposable();
+        this.cartDataSource = new LocalCartDataSource(CartDatabase.getInstance(context).cartDAO());
     }
 
     @NonNull
@@ -51,11 +65,11 @@ public class MyFoodListAdapter extends RecyclerView.Adapter<MyFoodListAdapter.My
         //Event
         holder.setListener((view, pos) -> {
             Common.selectedFood = foodModelList.get(pos);
-            Common.selectedFood .setKey(String.valueOf(pos));
+            Common.selectedFood.setKey(String.valueOf(pos));
             EventBus.getDefault().postSticky(new FoodItemClick(true, foodModelList.get(pos)));
         });
         //
-        holder.img_fav.setOnClickListener(v -> {
+        holder.img_quick_cart.setOnClickListener(v -> {
 
             CartItem cartItem = new CartItem();
             cartItem.setUid(Common.currentUser.getUid());
@@ -72,10 +86,20 @@ public class MyFoodListAdapter extends RecyclerView.Adapter<MyFoodListAdapter.My
 
             //5:16
 
+            compositeDisposable.add(cartDataSource
+                    .insertOrReplaceALL(cartItem)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(() -> {
+                                Toast.makeText(context, "Add to Cart success", Toast.LENGTH_SHORT).show();
+                                //Here we will send a notify to HomeActivity to update counter in cart
+                                EventBus.getDefault().postSticky(new CounterCartEvent(true));
+                            }, throwable -> {
+                                Toast.makeText(context, "[CART ERROR]" + throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                    )
+            );
         });
-
-
-
     }
 
     @Override
@@ -108,6 +132,7 @@ public class MyFoodListAdapter extends RecyclerView.Adapter<MyFoodListAdapter.My
         public void setListener(IRecyclerClickListener listener) {
             this.listener = listener;
         }
+
         @Override
         public void onClick(View v) {
             listener.onItemClickListener(v, getAdapterPosition());
--- app/src/main/java/com/cudpast/myeatapp/HomeActivity.java
+++ app/src/main/java/com/cudpast/myeatapp/HomeActivity.java
@@ -4,7 +4,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Toast;
 
+import com.andremion.counterfab.CounterFab;
+import com.cudpast.myeatapp.Commom.Common;
+import com.cudpast.myeatapp.Database.CartDataSource;
+import com.cudpast.myeatapp.Database.CartDatabase;
+import com.cudpast.myeatapp.Database.LocalCartDataSource;
 import com.cudpast.myeatapp.EventBus.CategoryClick;
+import com.cudpast.myeatapp.EventBus.CounterCartEvent;
 import com.cudpast.myeatapp.EventBus.FoodItemClick;
 import com.google.android.material.navigation.NavigationView;
 
@@ -20,18 +26,40 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.SingleObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class HomeActivity extends AppCompatActivity {
-    // 6.14
+
     AppBarConfiguration mAppBarConfiguration;
     NavController navController;
     NavigationView navigationView;
     DrawerLayout drawer;
     Toolbar toolbar;
 
+    private CartDataSource cartDataSource;
+
+    @BindView(R.id.fab)
+    CounterFab fab;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        countCartItem();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //
+        ButterKnife.bind(this);
+        cartDataSource = new LocalCartDataSource(CartDatabase.getInstance(this).cartDAO());
+        //
         drawer = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
@@ -42,10 +70,14 @@ public class HomeActivity extends AppCompatActivity {
                 R.id.nav_menu,
                 R.id.nav_food_details,
                 R.id.nav_food_list)
-                .setDrawerLayout(drawer).build();
+                .setDrawerLayout(drawer)
+                .build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        countCartItem();
+
     }
 
     @Override
@@ -84,6 +116,37 @@ public class HomeActivity extends AppCompatActivity {
         if (event.isSuccess()) {
             navController.navigate(R.id.nav_food_details);
         }
+    }
+
+    //8:19
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    public void onCartCounter(CounterCartEvent event) {
+        if (event.isSuccess()) {
+            countCartItem();
+        }
+    }
+
+    private void countCartItem() {
+        cartDataSource
+                .countItemInCart(Common.currentUser.getUid())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(Integer integer) {
+                        fab.setCount(integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(HomeActivity.this, "[Count cart]" +e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 }
