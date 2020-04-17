2bbc66915dc1d788043bc041334d94189045ab59
Slinkter <luis.j.cueva@gmail.com>
2020-04-16 20:48:57 -0500

    v13a:ok

--- app/src/main/java/com/cudpast/myeatapp/Database/CartDataSource.java
+++ app/src/main/java/com/cudpast/myeatapp/Database/CartDataSource.java
@@ -1,4 +1,25 @@
 package com.cudpast.myeatapp.Database;
 
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+
 public interface CartDataSource {
+
+    Flowable<List<CartItem>> getAllCart(String uid);
+    Single<Integer> countItemInCart(String uid);
+    Single<Long> sumPriceInCart(String uid);
+    Single<CartItem> getItemInCart(String foodId, String uid);
+    Completable insertOrReplaceALL(CartItem... cartItems);
+    Single<Integer> updateCartItems(CartItem cartItem);
+    Single<Integer> deleteCartItem(CartItem cartItem);
+    Single<Integer> cleanCart(String uid);
 }
